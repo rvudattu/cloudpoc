d228e14ca22d0ac6e2fba6da7b0a3f51209194c3
Raj Vihari V <93786419+rvudattu@users.noreply.github.com>
2023-02-08 18:22:38 +0530

    Update views.py

--- allocation/views.py
+++ allocation/views.py
@@ -26,7 +26,7 @@ def count_cur_sim(request):
         # Added a comment on 11-01-2023_14:43
 
         
-        return JsonResponse({"status": "success", "data": "Response version-1.17"})
+        return JsonResponse({"status": "success", "data": request.data})
 
 
     except Exception as e:
